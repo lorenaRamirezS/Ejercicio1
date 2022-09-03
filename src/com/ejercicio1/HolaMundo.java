804af082c6d561897a3b1d7d235ac320c3cdc215
lorena <ramirezserranob95@gmail.com>
2022-09-03 14:53:20 +0200

    realizando primer ejercicio

---
+++ src/com/ejercicio1/HolaMundo.java
@@ -0,0 +1,10 @@
+package com.ejercicio1;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");
+	}
+
+}
---
+++ src/com/ejercicio1/TiposDeDatos.java
@@ -0,0 +1,47 @@
+package com.ejercicio1;
+
+public class TiposDeDatos {
+	public static void main(String[] args) {
+		
+		// Numericos
+		// 1.1 enteros
+
+		byte variable1 = 5;
+		short variable2 = 10;
+		int variable3 = 30;
+		long variable4 = 100;
+
+		// 1.2 decimales
+
+		float variable5 = 5.5f;
+		double variable6 = 10.5d;
+
+		// 2 booleano
+
+		boolean variable7 = false;
+		boolean variable8 = true;
+
+		// 3 texto
+
+		char variable9 = 'a';
+		String variable10 = "Hola mundo";
+		
+		System.out.println("Este es un dato Numerico Entero y su valor es " + variable1);
+		System.out.println("Este es un dato Numerico Entero y su valor es " + variable2);
+		System.out.println("Este es un dato Numerico Entero y su valor es " + variable3);
+		System.out.println("Este es un dato Numerico Entero y su valor es " + variable4);
+		
+		System.out.println("Este es un dato Numerico Decimal y su valor es " + variable5);
+		System.out.println("Este es un dato Numerico Decimal y su valor es " + variable6);
+		
+		System.out.println("Este es un dato Booleano " + variable7);
+		System.out.println("Este es un dato Numerico Booleano " + variable8);
+		
+		System.out.println("Este es un dato Char " + variable9);
+		System.out.println("Este es un dato String " + variable10);
+		
+		
+		
+		
+	}
+}
